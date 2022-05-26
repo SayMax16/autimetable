10c66d44abe87f262bf4fcfd0f90fd08705e7fe2
SAMODIL <ab9054837@akfauniversity.org>
2022-05-26 10:52:53 +0500

    table view

---
+++ AKFA Timetable/Model/SheetManager.swift
@@ -0,0 +1,61 @@
+//
+//  SheetManager.swift
+//  AKFA Timetable
+//
+//  Created by Saydullo Ismatov on 26/05/22.
+//
+
+import Foundation
+
+protocol SheetManagerDelegate{
+    func didUpdateSheet(_  sheetManager: SheetManager, sheet: SheetModel)
+    func didFailWithError(error: Error )
+}
+
+struct SheetManager{
+    var apiURL = "https://sheet.best/api/sheets/b27766d2-9967-4994-ad41-e0603af73992#"
+    
+    var delegate: SheetManagerDelegate?
+    
+    func fetchSheet(){
+        performRequest(apiURL)
+    }
+    
+    func performRequest(_ urlString: String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default )
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                     return
+                }
+                 
+                if let safeData = data {
+                    print(safeData)
+                    if let sheet = self.parseJSON(safeData){
+                        self.delegate?.didUpdateSheet(self, sheet: sheet)
+                    }
+                    
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ sheet: Data ) -> SheetModel?{
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode([SheetData].self, from: sheet)
+            let name = decodedData[0].Full_name
+            let id = decodedData[0].Student_ID
+            decodedData.forEach { course in print(course)  }
+            let sheet = SheetModel(name: name, id: id)
+            return sheet
+            
+            
+        } catch {
+            self.delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+}
---
+++ AKFA Timetable/Model/SheetModel.swift
@@ -0,0 +1,14 @@
+//
+//  SheetModel.swift
+//  AKFA Timetable
+//
+//  Created by Saydullo Ismatov on 26/05/22.
+//
+
+import Foundation
+
+struct SheetModel{
+    let name:String
+    let id: String
+
+}
